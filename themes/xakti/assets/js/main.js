6046023ab534128e49e1b4c704a187440ab0eee9
Farid Efendi <miftahfaridse@gmail.com>
2019-09-12 13:10:46 +0700

    Add loader on blank loc

--- themes/xakti/assets/js/main.js
+++ themes/xakti/assets/js/main.js
@@ -5,4 +5,18 @@
 
     $(window).on('load', function(){$('.loader-bg').hide();});
 
+    $('.ajax-link').click(function () {
+        var href = $(this).attr('href');
+        $('.loader-bg').show();
+        if ($('.mdl-layout__drawer').hasClass('is-visible')) {
+            $('.mdl-layout__drawer').removeClass('is-visible');
+        }
+
+        $('.mdl-layout__obfuscator').addClass('is-visible');
+        console.log(href);
+        window.location.href = href;
+
+        return false;
+    });
+
 }());
